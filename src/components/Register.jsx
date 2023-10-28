8b6bd2a03fcc0cb6c8c8ba02014f4b1d2d7f744b
akashtharuka <akash_tharuka@yahoo.com>
2023-10-28 17:31:36 +0530

    register + login styling

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -3,6 +3,8 @@ import { useState, useEffect } from 'react';
 import { HashLink } from 'react-router-hash-link';
 import axios from '../api/axios';
 
+import { images } from '../javascript/imageImports.js';
+
 const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/;
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
 const EMAIL_REGEX = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
@@ -118,86 +120,105 @@ const Register = () => {
                     </div>
                 </section>
             ) : (
-                <div className='register container-fluid col-4'>
-                    <div className="card bg-light mx-auto px-md-5 px-1 py-5">
-                        <p className={`mx-auto ${errMsg ? "errmsg" : "offscreen"}`}>{errMsg}</p>
-                        <div className="card-title display-6 text-uppercase text-center">
-                            Register
+                <div className='register container-fluid col-12 px-0' style={{backgroundImage: `url(${images.bg_jungle_landscape})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat'}}>
+                    <div className="card bg-light mx-auto border-0 rounded-5 shadow col-8 d-flex flex-row position-absolute">
+                        <div className="title-section col-6">
+                            <div className="img-container">
+                                <img className='img-fluid p-5' src={images.signup} alt="register" />
+                            </div>
                         </div>
-                        <div className="card-body">
-                            <form className='form needs-validation' onSubmit={handleSubmit} noValidate>
-                                <label className='form-label' htmlFor="username">Username</label>
-                                <input 
-                                    type="text"
-                                    className={`form-control ${!user ? "" : validName ? "is-valid" : "is-invalid"}`}
-                                    id="username"
-                                    autoComplete='off'
-                                    onChange={e => setUser(e.target.value)}
-                                    value={user}
-                                    required
-                                    onFocus={() => setUserFocus(true)}
-                                    onBlur={() => setUserFocus(false)} 
-                                />
-                                <p id="uidnote" className={userFocus && user && !validName ? "invalid-feedback" : "offscreen"}>
-                                    4 to 24 characters. <br />
-                                    Must begin with a letter. <br />
-                                    Letters, numbers, underscores, hyphens allowed
-                                </p>
+                        <div className="form-section col-6 text-center">
+                            <p className={`mx-auto ${errMsg ? "errmsg" : "offscreen"}`}>{errMsg}</p>
+                            <div className="card-title">
+                                <h1 className='text-uppercase p-5'>Register</h1>
+                            </div>
+                            <div className="card-body px-5">
+                                <form className='form needs-validation' onSubmit={handleSubmit} noValidate>
+                                    <div className="form-floating text-secondary mb-4">
+                                        <input 
+                                            type="text"
+                                            className={`form-control border-0 rounded-5 ${!user ? "" : validName ? "is-valid" : "is-invalid"}`}
+                                            id="username"
+                                            autoComplete='off'
+                                            onChange={e => setUser(e.target.value)}
+                                            value={user}
+                                            required
+                                            onFocus={() => setUserFocus(true)}
+                                            onBlur={() => setUserFocus(false)}
+                                            placeholder='exampleUsername'
+                                        />
+                                        <label className='form-label' htmlFor="username">Username</label>
+                                        <p id="uidnote" className={userFocus && user && !validName ? "invalid-feedback text-start" : "offscreen"}>
+                                            4 to 24 characters. <br />
+                                            Must begin with a letter. <br />
+                                            Letters, numbers, underscores, hyphens allowed
+                                        </p>
+                                    </div>
 
-                                <label className='form-label' htmlFor="email">Email</label>
-                                <input 
-                                    type="email"
-                                    className={`form-control ${!email ? "" : validEmail ? "is-valid" : "is-invalid"}`}
-                                    id="email"
-                                    autoComplete='off'
-                                    onChange={e => setEmail(e.target.value)}
-                                    value={email}
-                                    required
-                                    onFocus={() => setEmailFocus(true)}
-                                    onBlur={() => setEmailFocus(false)} 
-                                />
-                                <p id="emailnote" className={emailFocus && email && !validEmail ? "invalid-feedback" : "offscreen"}>
-                                    Not a valid Email
-                                </p>
+                                    <div className="form-floating text-secondary mb-4">
+                                        <input 
+                                            type="email"
+                                            className={`form-control border-0 rounded-5 ${!email ? "" : validEmail ? "is-valid" : "is-invalid"}`}
+                                            id="email"
+                                            autoComplete='off'
+                                            onChange={e => setEmail(e.target.value)}
+                                            value={email}
+                                            required
+                                            onFocus={() => setEmailFocus(true)}
+                                            onBlur={() => setEmailFocus(false)} 
+                                            placeholder='example@email.com'
+                                        />
+                                        <label className='form-label' htmlFor="email">Email</label>
+                                        <p id="emailnote" className={emailFocus && email && !validEmail ? "invalid-feedback text-start" : "offscreen"}>
+                                            Not a valid Email
+                                        </p>
+                                    </div>
 
-                                <label className='form-label' htmlFor="password">Password</label>
-                                <input 
-                                    type="password"
-                                    className={`form-control ${!pwd ? "" : validPwd ? "is-valid" : "is-invalid"}`}
-                                    id="password"
-                                    onChange={e => setPwd(e.target.value)}
-                                    required
-                                    onFocus={() => setPwdFocus(true)}
-                                    onBlur={() => setPwdFocus(false)}
-                                />
-                                <p id="pwdnote" className={pwdFocus && !validPwd ? "invalid-feedback" : "offscreen"}>
-                                    8 to 24 characters. <br />
-                                    Must include uppercase and lowercase letters, a number and a special character. <br />
-                                    Allowed special characters: <span aria-label="exclamation mark">!</span> <span aria-label="at symbol">@</span> <span aria-label="hashtag">#</span> <span aria-label="dollar sign">$</span> <span aria-label="percent">%</span>
-                                </p>
+                                    <div className="form-floating text-secondary mb-4">
+                                        <input 
+                                            type="password"
+                                            className={`form-control border-0 rounded-5 ${!pwd ? "" : validPwd ? "is-valid" : "is-invalid"}`}
+                                            id="password"
+                                            onChange={e => setPwd(e.target.value)}
+                                            required
+                                            onFocus={() => setPwdFocus(true)}
+                                            onBlur={() => setPwdFocus(false)}
+                                            placeholder='examplePassword'
+                                        />
+                                        <label className='form-label' htmlFor="password">Password</label>
+                                        <p id="pwdnote" className={pwdFocus && !validPwd ? "invalid-feedback text-start" : "offscreen"}>
+                                            8 to 24 characters. <br />
+                                            Must include uppercase and lowercase letters, a number and a special character. <br />
+                                            Allowed special characters: <span aria-label="exclamation mark">!</span> <span aria-label="at symbol">@</span> <span aria-label="hashtag">#</span> <span aria-label="dollar sign">$</span> <span aria-label="percent">%</span>
+                                        </p>
+                                    </div>
 
-                                <label className='form-label' htmlFor="matchPwd">Confirm Password</label>
-                                <input 
-                                    type="password"
-                                    className={`form-control ${!matchPwd ? "" : validMatchPwd ? "is-valid" : "is-invalid"}`}
-                                    id="matchPwd"
-                                    onChange={e => setMatchPwd(e.target.value)}
-                                    required
-                                    onFocus={() => setMatchPwdFocus(true)}
-                                    onBlur={() => setMatchPwdFocus(false)}
-                                />
-                                <p id="matchPwdNote" className={matchPwdFocus && !validMatchPwd ? "invalid-feedback" : "offscreen"}>
-                                    Must match the first password input field.
-                                </p>
+                                    <div className="form-floating text-secondary mb-4">
+                                        <input 
+                                            type="password"
+                                            className={`form-control border-0 rounded-5 ${!matchPwd ? "" : validMatchPwd ? "is-valid" : "is-invalid"}`}
+                                            id="matchPwd"
+                                            onChange={e => setMatchPwd(e.target.value)}
+                                            required
+                                            onFocus={() => setMatchPwdFocus(true)}
+                                            onBlur={() => setMatchPwdFocus(false)}
+                                            placeholder='exampleMatchPassword'
+                                        />
+                                        <label className='form-label' htmlFor="matchPwd">Confirm Password</label>
+                                        <p id="matchPwdNote" className={matchPwdFocus && !validMatchPwd ? "invalid-feedback text-start" : "offscreen"}>
+                                            Must match the first password input field.
+                                        </p>
+                                    </div>
 
-                                <button className='col-12 btn btn-outline-dark mt-5' disabled={!validName || !validPwd || !validMatchPwd ? true : false}>Sign Up</button>
-                            </form>
-                            <p className='mt-4'>
-                                Already Registered? <br />
-                                <span>
-                                    <HashLink className='text-decoration-none' to="/login">Sign In</HashLink>
-                                </span>
-                            </p>
+                                    <button className='col-4 btn btn-outline-dark mt-2 rounded-5 p-2' disabled={!validName || !validPwd || !validMatchPwd ? true : false}>Sign Up</button>
+                                </form>
+                                <p className='mt-4'>
+                                    Already Registered? <br />
+                                    <span>
+                                        <HashLink className='text-decoration-none' to="/login">Sign In</HashLink>
+                                    </span>
+                                </p>
+                            </div>
                         </div>
                     </div>
                 </div>
---
+++ src/javascript/imageImports.js
@@ -0,0 +1,11 @@
+import bg_1 from '../assets/images/grey_padlock_and_laptop_wallpaper.jpg';
+import signup from '../assets/images/signup_illustration.svg';
+import login from '../assets/images/login_illustration.svg';
+import bg_jungle_landscape from '../assets/images/jungle_landscape.jpeg';
+import bg_jungle_landscape_2 from '../assets/images/jungle_landscape_2.jpeg';
+
+export const images = {
+    bg_1,
+    signup, login,
+    bg_jungle_landscape, bg_jungle_landscape_2,
+};

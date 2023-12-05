b893f24d5ad98edc166ff2e70a8fc31fc46506b0
akashtharuka <akash_tharuka@yahoo.com>
2023-12-05 12:05:27 +0530

    success notification card styles

--- src/components/SuccessNotification.jsx
+++ src/components/SuccessNotification.jsx
@@ -4,14 +4,18 @@ import { images } from '../javascript/imageImports';
 
 const SuccessNotification = () => {
     return (
-        <div className="success container-fluid col-12 px-0" style={{backgroundImage: `url(${images.bg_jungle_landscape_3})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat'}}>
-            <div className="success card bg-light mx-auto border-0 rounded-5 shadow col-lg-4 col-md-6 col-sm-8 position-absolute text-center d-flex">
-                <div className="card-body">
-                    <h1 className='card-title text-uppercase p-5'>Success!</h1>
-                    <img className='check-mark img-fluid mx-auto' src={images.check} alt="green check mark" />
-                    <HashLink className='text-decoration-none' to="/login">
-                        <button className="btn btn-dark p-2 px-4 m-5">Sign In</button>
-                    </HashLink>
+        <div className="success container-fluid col-12 row m-0 p-0 position-relative" style={{backgroundImage: `url(${images.abstract_bg_6})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat'}}>
+            <div className="row m-0 p-0">
+                <div className="col-12 col-sm-10 m-0 p-0 mx-auto d-flex align-items-center">
+                    <div className="success-card card bg-light m-0 mx-auto px-sm-3 border-0 rounded-5 shadow col-10 col-md-6 col-lg-4 text-center">
+                        <div className="card-body d-flex flex-column justify-content-center">
+                            <h1 className='card-title text-uppercase'>Success!</h1>
+                            <img className='check-mark img img-fluid mx-auto my-5' src={images.check} alt="green check mark" />
+                            <HashLink className='text-decoration-none' to="/login">
+                                <button className="btn btn-dark p-2 px-4 my-3">Sign In</button>
+                            </HashLink>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>

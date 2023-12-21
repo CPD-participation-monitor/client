fefb1f6df224694f577e3546ae10a3dd28d656a4
akashtharuka <akash_tharuka@yahoo.com>
2023-12-22 04:25:33 +0530

    logout button fix - styles & container styles fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,8 @@ import { HOME_ROUTE, LOGIN_ROUTE, ENG_DASHBOARD_ROUTE, ADMIN_DASHBOARD_ROUTE, CP
 import { roles } from "../utils/constants";
 import { useSelector, useDispatch } from "react-redux";
 import { logout, reset } from "../features/auth/authSlice";
+import { ArrowRightStartOnRectangleIcon } from "@heroicons/react/24/outline";
+import { IconButton } from "@material-tailwind/react";
 
 const Navbar = () => {
 
@@ -23,38 +25,45 @@ const Navbar = () => {
     }, [user]);
 
     return (
-        <div className="container mx-auto">
-            <div className="navbar bg-base-100">
-                <div className="navbar-start">
-                    <div className="dropdown">
-                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                        </div>
-                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                            <li className="mx-4"><Link to={HOME_ROUTE}>Home</Link></li>
-                            <li className="mx-4"><Link to={CPD_DASHBOARD_ROUTE}>Find CPD</Link></li>
-                            <li className="mx-4"><Link to={navLinkDashboard}>Dashboard</Link></li>
-                        </ul>
+        <div className="navbar md:container mx-auto bg-base-100 md:px-16 border-b-2 border-slate-100">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </div>
-                    <Link to={HOME_ROUTE} className="btn btn-ghost text-xl text-center">CPD Monitor</Link>
-                </div>
-                <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal px-1">
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
                         <li className="mx-4"><Link to={HOME_ROUTE}>Home</Link></li>
                         <li className="mx-4"><Link to={CPD_DASHBOARD_ROUTE}>Find CPD</Link></li>
                         <li className="mx-4"><Link to={navLinkDashboard}>Dashboard</Link></li>
                     </ul>
                 </div>
-                {user ? (
-                    <div className="navbar-end">
-                        <button className="btn md:btn-wide bg-red-500" onClick={onLogout}>Logout</button>
-                    </div>
-                ) : (
-                    <div className="navbar-end">
-                        <Link to={LOGIN_ROUTE} className="btn md:btn-wide">Login</Link>
-                    </div>
-                )}
+                <Link to={HOME_ROUTE} className="btn btn-ghost text-xl text-center px-0">CPD Monitor</Link>
             </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    <li className="mx-4"><Link to={HOME_ROUTE}>Home</Link></li>
+                    <li className="mx-4"><Link to={CPD_DASHBOARD_ROUTE}>Find CPD</Link></li>
+                    <li className="mx-4"><Link to={navLinkDashboard}>Dashboard</Link></li>
+                </ul>
+            </div>
+            {user ? (
+                <>
+                    <div className="hidden md:flex navbar-end">
+                        <button className="btn bg-red-500 text-slate-800" onClick={onLogout}>Logout
+                            <ArrowRightStartOnRectangleIcon className="w-4 h-4" />
+                        </button>
+                    </div>
+                    <div className="md:hidden navbar-end ">
+                        <IconButton variant='gradient' onClick={onLogout} color='red'>
+                            <ArrowRightStartOnRectangleIcon className="w-4 h-4" />
+                        </IconButton>
+                    </div>
+                </>
+            ) : (
+                <div className="navbar-end">
+                    <Link to={LOGIN_ROUTE} className="btn md:btn-wide">Login</Link>
+                </div>
+            )}
         </div>
     )
 }

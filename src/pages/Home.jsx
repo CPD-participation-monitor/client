2bbd1354e6721eec7afa5c5479525294834a034d
akashtharuka <akash_tharuka@yahoo.com>
2023-12-25 10:38:17 +0530

    home page basic UI

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,26 @@
+import { Typography } from "@material-tailwind/react";
+import imageImports from "../utils/imageImports";
+
 const Home = () => {
   return (
-    <div className="container mx-auto p-4 md:p-0">
-        Home
+    <div className="md:container mx-auto px-4 md:px-16 py-8">
+      <section className="hero-section grid grid-cols-1 md:grid-cols-2">
+        <div className="hero-text flex flex-col justify-center">
+          <Typography variant='h1' color='blue-gray' className='mb-4'>
+            Welcome to the CPD Portal
+          </Typography>
+          <Typography variant='paragraph' color='blue-gray'>
+            The CPD Portal is a platform that allows CPD providers to create events and CPD participants to join those events.
+          </Typography>
+        </div>
+        <div className="hero-image">
+          <img
+            className="h-full w-full rounded-lg object-cover object-center"
+            src={imageImports.heroImage}
+            alt="hero section image"
+          />
+        </div>
+      </section>
     </div>
   );
 }
---
+++ src/utils/imageImports.js
@@ -0,0 +1,8 @@
+import heroImage from '../assets/images/hero-image-removebg.png';
+
+
+const imageImports = {
+    heroImage,
+};
+
+export default imageImports;

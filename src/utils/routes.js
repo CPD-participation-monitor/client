88867e4a4e4216ac9c67254fe822b4737b3edee8
akashtharuka <akash_tharuka@yahoo.com>
2023-12-26 11:36:43 +0530

    added ENG_DASHBOARD_ROUTE

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -3,5 +3,6 @@ export const ENG_DASHBOARD_ROUTE = '/engdashboard';
 export const ADMIN_DASHBOARD_ROUTE = '/admindashboard';
 export const CPD_DASHBOARD_ROUTE = '/cpddashboard';
 export const ORG_DASHBOARD_ROUTE = '/orgdashboard/:orgID';
+export const EVENT_DASHBOARD_ROUTE = '/eventdashboard/:eventID';
 export const LOGIN_ROUTE = '/login';
 export const REGISTER_ROUTE = '/register';
